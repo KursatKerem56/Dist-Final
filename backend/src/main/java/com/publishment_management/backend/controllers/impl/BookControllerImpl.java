e30f416ef8f0388d947249a64e553d2c37ceaa21
AlperYildrm <alperyildirim5552@hotmail.com>
2026-05-01 13:45:41 +0300

    Basic Backend Completed
    The rest is image upload/download and pdf generation

--- backend/src/main/java/com/publishment_management/backend/controllers/impl/BookControllerImpl.java
+++ backend/src/main/java/com/publishment_management/backend/controllers/impl/BookControllerImpl.java
@@ -3,46 +3,62 @@ package com.publishment_management.backend.controllers.impl;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.publishment_management.backend.controllers.IBookController;
+import com.publishment_management.backend.controllers.RestBaseController;
 import com.publishment_management.backend.dtos.DtoBook;
 import com.publishment_management.backend.dtos.DtoBookIU;
 import com.publishment_management.backend.models.RootEntity;
+import com.publishment_management.backend.services.IBookService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "api/v1/book")
-public class BookControllerImpl implements IBookController{
+public class BookControllerImpl extends RestBaseController implements IBookController{
 
+	private final IBookService bookService;
+	
 	@Override
+	@GetMapping(path = "/list")
 	public ResponseEntity<RootEntity<List<DtoBook>>> getBookList() {
-		// TODO Auto-generated method stub
-		return null;
+		return ok(bookService.getBookList());
 	}
 
 	@Override
-	public ResponseEntity<RootEntity<DtoBook>> getBookById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<RootEntity<DtoBook>> getBookById(@PathVariable Integer id) {
+		return ok(bookService.getBookById(id));
 	}
 
 	@Override
-	public ResponseEntity<RootEntity<DtoBook>> saveBook(DtoBookIU book) {
-		// TODO Auto-generated method stub
-		return null;
+	@PostMapping(path = "/list")
+	public ResponseEntity<RootEntity<DtoBook>> saveBook(@Valid @RequestBody DtoBookIU book) {
+		return ok(bookService.saveBook(book));
 	}
 
 	@Override
-	public ResponseEntity<RootEntity<Void>> deleteBook(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	@DeleteMapping(path = "/delete/{id}")
+	public ResponseEntity<RootEntity<Void>> deleteBook(@PathVariable Integer id) {
+		bookService.deleteBook(id);
+		return ok(null);
 	}
+	
 
 	@Override
-	public ResponseEntity<RootEntity<DtoBook>> changeBook(Integer id, DtoBookIU book) {
-		// TODO Auto-generated method stub
-		return null;
+	@PutMapping(path = "/change{id}")
+	public ResponseEntity<RootEntity<DtoBook>> changeBook(@PathVariable Integer id,@Valid @RequestBody DtoBookIU book) {
+		return ok(bookService.changeBook(id, book));
 	}
 	
 	
--- backend/src/main/java/com/publishment_management/backend/controllers/impl/PublishesControllerImpl.java
+++ backend/src/main/java/com/publishment_management/backend/controllers/impl/PublishesControllerImpl.java
@@ -3,46 +3,60 @@ package com.publishment_management.backend.controllers.impl;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.publishment_management.backend.controllers.IPublishesController;
+import com.publishment_management.backend.controllers.RestBaseController;
 import com.publishment_management.backend.dtos.DtoPublishes;
 import com.publishment_management.backend.dtos.DtoPublishesIU;
 import com.publishment_management.backend.models.RootEntity;
+import com.publishment_management.backend.services.IPublishesService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "api/v1/publishes")
-public class PublishesControllerImpl implements IPublishesController {
+@RequiredArgsConstructor
+public class PublishesControllerImpl extends RestBaseController implements IPublishesController {
 
+	private final IPublishesService publishesService;
+	
 	@Override
+	@GetMapping(path = "/{id}")
 	public ResponseEntity<RootEntity<DtoPublishes>> getPublishesById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ok(publishesService.getPublishesById(id));
 	}
 
 	@Override
+	@GetMapping(path = "/list")
 	public ResponseEntity<RootEntity<List<DtoPublishes>>> getPublishesList() {
 		// TODO Auto-generated method stub
-		return null;
+		return ok(publishesService.getPublishesList());
 	}
 
 	@Override
+	@PostMapping(path = "/list")
 	public ResponseEntity<RootEntity<DtoPublishes>> savePublishes(DtoPublishesIU publishes) {
-		// TODO Auto-generated method stub
-		return null;
+		return ok(publishesService.savePublishes(publishes));
 	}
 
 	@Override
+	@DeleteMapping(path = "/delete/{id}")
 	public ResponseEntity<RootEntity<Void>> deletePublishes(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		publishesService.deletePublishes(id);
+		return ok(null);
 	}
 
 	@Override
+	@PutMapping(path = "/change/{id}")
 	public ResponseEntity<RootEntity<DtoPublishes>> changePublishes(Integer id, DtoPublishesIU changedPublishes) {
 		// TODO Auto-generated method stub
-		return null;
+		return ok(publishesService.changePublishes(id, changedPublishes));
 	}
 	
 	
